c84f728b1d6b88ac6e9dcb2e0c657591c5432c9e
ThomasDickson <thomas.dickson13@outlook.com>
2022-12-12 13:03:25 +1100

    commit on main

---
+++ src/assets/photos.js
@@ -0,0 +1,27 @@
+export const photos = [
+    {
+      src: 'https://www.brwnpaperbag.com/wp-content/uploads/2020/05/the-magic-shop-ceramics-1.jpg',
+      height: 2,
+      width: 3
+    },  
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0502/8682/1530/products/IMG_4521_1250x1250.jpg?v=1644969569',
+      height: 1,
+      width: 1
+    },
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0949/3484/products/art-sw-95_600x.jpg?v=1661837341',
+      height: 1,
+      width: 1
+    },
+    {
+      src: 'https://www.azzonealessandra.com/wp-content/uploads/2016/03/Rainbow-trees-mini-serving-plates_Publ-2.jpg',
+      height: 1024,
+      width: 768
+    },
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0949/3484/products/252_CERAMICS_WEB-RES_600x.jpg?v=1661838292',
+      height: 1,
+      width: 1
+    }
+];
