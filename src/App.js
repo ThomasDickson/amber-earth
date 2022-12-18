9c477d984322f237e9558062f4a11e8c896ea1e6
ThomasDickson <thomas.dickson13@outlook.com>
2022-12-18 16:55:48 +1100

    added routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
+import Shop from './components/pages/Shop';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Router>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Logo from '../assets/logo.svg'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 function Navbar() {
   return (
     <div id="navbar">
       <ul id="menu">
-        <span className="menuItem">HOME</span>
-        <span className="menuItem">SHOP</span>
-        <img id="logo" src={Logo} alt="amber earth logo" />
-        <span className="menuItem">ABOUT</span>
-        <span className="menuItem">CONTACT US</span>  
+        <Link to="/">
+          <img id="logo" src={Logo} alt="amber earth logo" />
+        </Link>
+        <Link className="menuItem" to="/shop">SHOP</Link>
+        <Link className="menuItem" to="/about">ABOUT</Link>
+        <Link className="menuItem" to="/contact">CONTACT US</Link>  
       </ul>    
     </div>
   )
